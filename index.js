de6c420bbf323d656ca5d2219308e7a2539af42f
mvoss9000 <mvoss9000@yahoo.com>
2017-06-01 23:40:33 -0400

    comment

--- index.js
+++ index.js
@@ -55,23 +55,24 @@ function obj(a1, a2) {
         Array.isArray(a1) || err('illegal argument: ' + a1)
         var i = 0
         if (a2) {
-            // create from keys (array) and vals (array or object)
             typeof a2 === 'object' || err('illegal argument: ' + a2)
             if (Array.isArray(a2)) {
+                // create from keys and vals arrays [ ['a', 'b', ...], [1, 2, ...] ]
                 var len = Math.min(a1.length, a2.length)
                 while(i < len) { ret.put(a1[i], a2[i]); i++ }
             } else {
+                // create from keys array and vals object [ ['a', 'b', ...], { a: 1, b: 2, ...} ]
                 while(i < a1.length) { ret.put(a1[i], a2[a1[i]]); i++ }
             }
         } else {
             // one argument
             switch (typeof a1[0]) {
                 case 'string':
-                    // create from array of alternating pairs
+                    // create from array of alternating pairs [ 'a': 1, 'b', 2, ... ]
                     while (i < a1.length) { ret.put(a1[i], a1[i+1]); i += 2 }
                     break
                 case 'object':
-                    // create from array of objects
+                    // create from array of objects  [ {a:1}, {b:2}, ... ]
                     a1.forEach(function (row) {
                         var keys = Object.keys(row)
                         keys.length === 1 || err('expected 1 item, but got: ' + keys)
